b2ffdef77cb045e08ec54161c482894c3ff24ac8
JojoLgenius <jocelyn.hauf@gmail.com>
2022-10-16 22:25:18 +0200

    Upgrade display

--- ressources.py
+++ ressources.py
@@ -1,11 +1,13 @@
 class item:
-    def __init__(self, value, weight):
+    def __init__(self, pos, value, weight):
+        self.pos = int(pos)
         self.value = float(value)
         self.weight = float(weight)
         self.ratio = float(value) / weight
 
     def printItem(self):
-        print("Value : " + str(self.value) + " | Weight : " + str(self.weight) + " Ratio : " + str(self.ratio))
+        #print("Value : " + str(self.value) + "| Weight : " + str(self.weight) + "Ratio : " + str(self.ratio) + " InitPos : " + str(self.pos))
+        print("Value : %-8.2f |\t Weight : %-8.2f |\tRatio : %-8.4f |\tInitPos : %-4d" % (self.value,self.weight,self.ratio ,self.pos))
 
 
 def readFileCreateList(path):
@@ -20,7 +22,9 @@ def readFileCreateList(path):
         words = firstline.split(" ")
         n = int(words[0])
         wmax = int(words[1])
+        i = 2
         for line in f:
             data = line.split(" ")
-            listItem.append(item(float(data[0]), float(data[1])))
+            listItem.append(item(i,float(data[0]), float(data[1])))
+            i += 1
     return n, wmax, listItem
