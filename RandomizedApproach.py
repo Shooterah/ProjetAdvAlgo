0094f809b7713eb2cbea37e019279438c023dd1d
Shooterah <iflox974@gmail.com>
2022-11-12 17:13:20 +0100

    Begining of the Randomized Approach

---
+++ RandomizedApproach.py
@@ -0,0 +1,15 @@
+
+# Knapsack Problem: Randomized Approach (Python)
+# Author: Florian Doffemont
+
+# Goal: Find a solution to the knapsack problem using a randomized approach.
+
+# It means that we will generate a random solution and check if it is better than the best solution found so far. If it is, we will keep it. If not, we will discard it and generate a new one. We will repeat this process until we have found a solution that is better than the best solution found so far. 
+
+# We will use the same data as the others algorithms.
+
+# Import the necessary libraries
+import random
+import time
+
+# Define the function that will generate a random solution
