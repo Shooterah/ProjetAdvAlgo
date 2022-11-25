6595b9ba2331788f49d35782620c176950b32160
jeoffrey42 <jeoffrey.pereira>
2022-11-25 14:05:58 +0100

    Ajout de fichier

--- DataGenerator.py
+++ DataGenerator.py
@@ -87,61 +87,81 @@ def createFile(wMax, vMax, n, filename) :
     file.close()
     return
 
-    
+# Create a file graphique
+def createFile_graphique(path,wMax, vMax, n, filename) :
+    filename = path + filename
+    file = open(filename, "w")
+    # First line of the file
+    file.write(str(n) + " " + str(wMax))
+    for i in range(n) :
+        file.write("\n")
+        line = createStringValues(wMax, vMax)
+        file.write(line)
+    file.close()
 
     
 
 
-#
-#  +------+
-#  | Main |
-#  +------+
-#
+def mainGraphique(path,filename,n,wMax):
+    
+    if filename == "" :
+        filename = "data.txt"
+    n = int(n)
+    wMax = int(wMax)
+    vMax = n * MULTV
+    createFile_graphique(path,wMax, vMax, n, str(filename))
+
+
+# #
+# #  +------+
+# #  | Main |
+# #  +------+
+# #
 
 
 
-#print("Begin")
+# #print("Begin")
 
 
-# Verify if the nb of arguments is correct
-if len(sys.argv)-1 < 2 :
-    print("Not enough arguments (" + str(len(sys.argv)) + "): ")
-    manual()
-    print("Program failed")
-    print("Exit")
-    exit()
+# # Verify if the nb of arguments is correct
+# if len(sys.argv)-1 < 2 :
+#     print("Not enough arguments (" + str(len(sys.argv)) + "): ")
+#     manual()
+#     print("Program failed")
+#     print("Exit")
+#     exit()
    
-# Define classic filename 
-filename = "data.txt"
+# # Define classic filename 
+# filename = "data.txt"
 
 
-# Take the values from the command line
-n = sys.argv[1]
-wMax = sys.argv[2]
+# # Take the values from the command line
+# n = sys.argv[1]
+# wMax = sys.argv[2]
 
-# Verify option to able filename
-if ((len(sys.argv)-1 > 3) and (sys.argv[3] == "-f")) :
-    filename = sys.argv[4]
+# # Verify option to able filename
+# if ((len(sys.argv)-1 > 3) and (sys.argv[3] == "-f")) :
+#     filename = sys.argv[4]
 
-# Set the (String) values to integers
-n = int(n)
-wMax = int(wMax)
+# # Set the (String) values to integers
+# n = int(n)
+# wMax = int(wMax)
 
-# vMax value (not to much)
-vMax = n * MULTV
+# # vMax value (not to much)
+# vMax = n * MULTV
 
-# File creation
-file = createFile(wMax, vMax, n, str(filename))
-
-
-## Print the arguments (Not necessary)
-#print("Values :")
-#print("n =" + str(n))
-#print("wMax = " + str(wMax))
-#print("vMax = " + str(vMax))
+# # File creation
+# file = createFile(wMax, vMax, n, str(filename))
 
 
-#print("End")
+# ## Print the arguments (Not necessary)
+# #print("Values :")
+# #print("n =" + str(n))
+# #print("wMax = " + str(wMax))
+# #print("vMax = " + str(vMax))
 
 
-exit()
+# #print("End")
+
+
+# exit()
--- Interface.py
+++ Interface.py
@@ -1,125 +1,288 @@
-# interface graphique avec Tkinter qui dispose de 8 boutons
-
+# Create a Window with Tkinter
+from asyncio import subprocess
 from tkinter import *
 import os
+import sys
+import subprocess
+import BruteForce
+import Branch_and_Bound
+import DataGenerator
+
+# Variable time and MaxValue from the programes
+time = [0 for x in range(8)]
+maxValue = [0 for x in range(8)]
 
 
-class Interface(Frame):
-
-    def __init__(self, fenetre, **kwargs):
-        Frame.__init__(self, fenetre, width=768, height=576, **kwargs)
-        self.pack(fill=BOTH)
-
-        # Création de nos widgets
-        self.message = Label(self, text="Interface graphique")
-        self.message.pack()
-
-        self.boutonQuitter = Button(self, text="Quitter", command=self.quit)
-        self.boutonQuitter.pack(side="bottom")
-
-        self.bouton1 = Button(self, text="Brute Force", command=self.bouton1)
-        self.bouton1.pack(side="left")
-
-        self.bouton2 = Button(self, text="Greedy 1", command=self.bouton2)
-        self.bouton2.pack(side="top")
-
-        self.bouton3 = Button(self, text="3", command=self.bouton3)
-        self.bouton3.pack(side="left")
-
-        self.bouton4 = Button(self, text="4", command=self.bouton4)
-        self.bouton4.pack(side="left")
-
-        self.bouton5 = Button(self, text="5", command=self.bouton5)
-        self.bouton5.pack(side="left")
-
-        self.bouton6 = Button(self, text="6", command=self.bouton6)
-        self.bouton6.pack(side="left")
-
-        self.bouton7 = Button(self, text="7", command=self.bouton7)
-        self.bouton7.pack(side="left")
-
-        self.bouton8 = Button(self, text="8", command=self.bouton8)
-        self.bouton8.pack(side="left")
-
-    def bouton1(self):
-        os.system("python /home/pi/Projet/Interface.py")
-
-    def bouton2(self):
-        os.system("python /home/pi/Projet/Interface2.py")
-
-    def bouton3(self):
-        os.system("python /home/pi/Projet/Interface3.py")
-
-    def bouton4(self):
-        os.system("python /home/pi/Projet/Interface4.py")
-
-    def bouton5(self):
-        os.system("python /home/pi/Projet/Interface5.py")
-
-    def bouton6(self):
-        os.system("python /home/pi/Projet/Interface6.py")
-
-    def bouton7(self):
-        os.system("python /home/pi/Projet/Interface7.py")
-
-    def bouton8(self):
-        os.system("python /home/pi/Projet/Interface8.py")
+# Function that call the program brute force with the file selected in the Entry fnright
+def callBruteForce():
+    # retrieve the file name
+    if problemType.get() == "simple":
+        path = "Data/low-dimensional/"+fnright.get()
+        time[0], maxValue[0] = BruteForce.main("simple", path)
+        print("time=", time[0], "maxValue=", maxValue[0])
+    elif problemType.get() == "multiDim":
+        path = "Data/multi-dimentional/"+fnright.get()
+        time[0], maxValue[0] = BruteForce.main("multi", path)
+        print("time=", time[0], "maxValue=", maxValue[0])
 
 
-# Création de la fenêtre principale
-fenetre = Tk()
-fenetre.geometry("768x576")
-fenetre.title("Interface graphique")
+def callgreedyv():
 
-# Création d'un widget Button (bouton Quitter)
-bouton_quitter = Button(fenetre, text="Quitter", command=fenetre.quit)
-bouton_quitter.pack(side="bottom")
-
-# Création d'un widget Button (bouton 1)
-bouton1 = Button(fenetre, text="Brute Force", command=fenetre.quit)
-bouton1.pack(fill=25 , ipady=20)
-
-# Création d'un widget Button (bouton 2)
-bouton2 = Button(fenetre, text="Greedy 1", command=fenetre.quit)
-bouton2.pack(fill=X, ipady=20)
-
-# Création d'un widget Button (bouton 3)
-bouton3 = Button(fenetre, text="3", command=fenetre.quit)
-bouton3.pack(fill=X, ipady=20)
-
-# Création d'un widget Button (bouton 4)
-bouton4 = Button(fenetre, text="4", command=fenetre.quit)
-bouton4.pack(fill=X, ipady=20)
-
-# Création d'un widget Button (bouton 5)
-bouton5 = Button(fenetre, text="5", command=fenetre.quit)
-bouton5.pack(fill=X, ipady=20)
-
-# Création d'un widget Button (bouton 6)
-bouton6 = Button(fenetre, text="6", command=fenetre.quit)
-bouton6.pack(fill=X, ipady=20)
-
-# Création d'un widget Button (bouton 7)
-bouton7 = Button(fenetre, text="7", command=fenetre.quit)
-bouton7.pack(fill=X, ipady=20)
-
-# Création d'un widget Button (bouton 8)
-bouton8 = Button(fenetre, text="8", command=fenetre.quit)
-bouton8.pack(fill=X, ipady=20)
+    # retrieve the file name
+    path = "Data/low-dimensional/"+fnright.get()
+    print("path=", path)
+    subprocess.run(["python", "Greedy.py", "-v",
+                   path])
 
 
-# Création d'un widget Label (texte)
-label = Label(fenetre, text="Interface graphique")
-label.pack()
-
-# Création d'un widget Canvas (zone graphique)
-zone = Canvas(fenetre, width=768, height=576, background="white")
-zone.pack()
-
-# Création d'un widget Frame (cadre)
-cadre = Frame(fenetre, borderwidth=2, relief=GROOVE)
-cadre.pack(side=LEFT, padx=10, pady=10)
+def callgreedyw():
+    # retrieve the file name
+    path = "Data/low-dimensional/"+fnright.get()
+    print("path=", path)
+    subprocess.run(["python", "Greedy.py", "-w", path])
 
 
-# Affichage du widget
-fenetre.mainloop()
+def callgreedyr():
+    # retrieve the file name
+    path = "Data/low-dimensional/"+fnright.get()
+    print("path=", path)
+    subprocess.run(["python", "Greedy.py", "-r", path])
+
+
+def callDynamic():
+    os.system("python Dynamic.py")
+
+
+def callBranchAndBound():
+    if problemType.get() == "simple":
+        path = "Data/low-dimensional/"+fnright.get()
+        time[5], maxValue[5] = Branch_and_Bound.main("simple", path)
+        print("time=", time[5], "maxValue=", maxValue[5])
+    elif problemType.get() == "multiDim":
+        path = "Data/multi-dimentional/"+fnright.get()
+        time[5], maxValue[5] = Branch_and_Bound.main("multi", path)
+        print("time=", time[5], "maxValue=", maxValue[5])
+
+
+# main
+window = Tk()
+# personalise the window
+window.title("Graphic Interface")
+window.geometry("1080x720")
+window.minsize(480, 360)
+window.config(background="#528860")
+
+# Create a menu bar to select the type of problem to solve (simple or multi-dimensional or multi-bag)if we click on the buttons, the variable problemType will be updated
+# The active button will be in green and the others in red
+# The variable problemType will be used to call the right program
+problemType = StringVar()
+problemType.set("simple")
+# Create a frame to contain the menu bar
+frameMenu = Frame(window, bg="#528860")
+frameMenu.pack(side=TOP, fill=X)
+# Create the buttons, one for each type of problem to solve if we click on the buttons, the variable problemType will be updated and the color of the buttons will be updated
+buttonSimple = Button(frameMenu, text="Simple", bg="green",
+                      fg="white", command=lambda: problemType.set("simple"))
+buttonSimple.pack(side=LEFT, padx=5, pady=5)
+buttonMultiDim = Button(frameMenu, text="Multi-Dimensional", bg="red",
+                        fg="white", command=lambda: problemType.set("multiDim"))
+buttonMultiDim.pack(side=LEFT, padx=5, pady=5)
+buttonMultiBag = Button(frameMenu, text="Multi-Bag", bg="red",
+                        fg="white", command=lambda: problemType.set("multiBag"))
+buttonMultiBag.pack(side=LEFT, padx=5, pady=5)
+
+#create 2 entry to write the number of items and the capacity of the knapsack inside the frame frameMenu
+#the variable items and capacity will be used to create a file with the right number of items and the right capacity
+items = StringVar()
+capacity = StringVar()
+filename = StringVar()
+# Create a frame to contain the entry
+frameEntry = Frame(window, bg="#528860")
+frameEntry.pack(side=TOP, fill=X)
+# Create the entry
+labelItems = Label(frameEntry, text="Number of items", bg="#528860", fg="white")
+labelItems.pack(side=LEFT, padx=5, pady=5)
+entryItems = Entry(frameEntry, textvariable=items, width=10)
+entryItems.pack(side=LEFT, padx=5, pady=5)
+labelCapacity = Label(frameEntry, text="Capacity", bg="#528860", fg="white")
+labelCapacity.pack(side=LEFT, padx=5, pady=5)
+entryCapacity = Entry(frameEntry, textvariable=capacity, width=10)
+entryCapacity.pack(side=LEFT, padx=5, pady=5)
+labelFilename = Label(frameEntry, text="Filename", bg="#528860", fg="white")
+labelFilename.pack(side=LEFT, padx=5, pady=5)
+entryFilename = Entry(frameEntry, textvariable=filename, width=10)
+entryFilename.pack(side=LEFT, padx=5, pady=5)
+# creat the entry of the file name
+# Create a button to create a file with the right number of items and the right capacity
+# The file will be created in the folder Data/low-dimensional
+# The name of the file will be the number of items and the capacity
+# The file will be created only if the number of items and the capacity are not empty
+def createFile_interface():
+    if items.get() != "" and capacity.get() != "":
+        if problemType.get() == "simple":
+            path = "Data/low-dimensional/"
+        elif problemType.get() == "multiDim":
+            path = "Data/multi-dimentional/"
+        elif problemType.get() == "multiBag":
+            path = "Data/multi-bag/"
+        DataGenerator.mainGraphique(path,filename.get(),int(items.get()), int(capacity.get()))
+        changeFiles()
+
+    else:
+        print("Please enter the number of items and the capacity")
+buttonCreateFile = Button(frameEntry, text="Create file", bg="green",
+                          fg="white", command=createFile_interface)
+buttonCreateFile.pack(side=LEFT, padx=5, pady=5)
+
+
+
+
+
+# change the color of the button when the variable problemType is updated
+
+
+def changeColor():
+    if problemType.get() == "simple":
+        buttonSimple.config(bg="green")
+        buttonMultiDim.config(bg="red")
+        buttonMultiBag.config(bg="red")
+    elif problemType.get() == "multiDim":
+        buttonSimple.config(bg="red")
+        buttonMultiDim.config(bg="green")
+        buttonMultiBag.config(bg="red")
+    elif problemType.get() == "multiBag":
+        buttonSimple.config(bg="red")
+        buttonMultiDim.config(bg="red")
+        buttonMultiBag.config(bg="green")
+
+
+# bind the function changeColor to the variable problemType
+problemType.trace("w", lambda *args: changeColor())
+
+
+# create 2 vertical frames that take boyh 50% of the window
+frame_left = Frame(window, bg="#528860", bd=1, relief=SUNKEN)
+frame_left.pack(side=LEFT, fill=BOTH)
+
+frame_right = Frame(window, bg="#8BC49A", bd=1, relief=SUNKEN)
+frame_right.pack(side=RIGHT, fill=BOTH, expand=YES)
+
+# label for file name
+fnleft = Label(frame_left, text="Select the file to solve :",
+               bg="#207436", fg="black", font=25)
+fnleft.grid(row=0, column=0, rowspan=2, columnspan=1, sticky=NSEW)
+
+# Mettre la liste des fichiers dans un menu déroulant de taille égalent à la taille du label fnleft et collé adroite de celle-ci.
+# Si la variable problemType est égale à "simple", on affiche les fichiers du dossier "Data/low-dimensional"
+# Si la variable problemType est égale à "multiDim", on affiche les fichiers du dossier "Data/multi-dimensional"
+# Si la variable problemType est égale à "multiBag", on affiche les fichiers du dossier "Data/multi-bag"
+
+if problemType.get() == "simple":
+    files = os.listdir("Data/low-dimensional")
+elif problemType.get() == "multiDim":
+    files = os.listdir("Data/multi-dimentional")
+elif problemType.get() == "multiBag":
+    files = os.listdir("Data/multi-bag")
+
+# create a variable to store the file name
+fnright = StringVar()
+# create a menu to select the file name
+fnright.set(files[0])
+fnrightMenu = OptionMenu(frame_left, fnright, *files)
+fnrightMenu.grid(row=0, column=1, rowspan=2, columnspan=2, sticky=NSEW)
+
+# Change the list of files in the menu when the variable problemType is updated
+
+
+def changeFiles():
+    if problemType.get() == "simple":
+        files = os.listdir("Data/low-dimensional")
+    elif problemType.get() == "multiDim":
+        files = os.listdir("Data/multi-dimentional")
+    elif problemType.get() == "multiBag":
+        files = os.listdir("Data/multi-bag")
+    fnrightMenu["menu"].delete(0, "end")
+    for file in files:
+        fnrightMenu["menu"].add_command(
+            label=file, command=lambda value=file: fnright.set(value))
+
+
+# bind the function changeFiles to the variable problemType
+problemType.trace("w", lambda *args: changeFiles())
+
+
+# determine a grid of 13*7 in the left frame
+for i in range(13):
+    frame_left.rowconfigure(i, weight=1)
+for i in range(7):
+    frame_left.columnconfigure(i, weight=1)
+
+
+# create 11 buttons in the left frame and each for one row and all columns
+# call brute force
+button1 = Button(frame_left, text="Brute Force",
+                 bg="#8BC49A", fg="black", bd=1, command=callBruteForce)
+button1.grid(row=3, column=0, columnspan=3, sticky=NSEW)
+
+button2 = Button(frame_left, text="Greedy Value",
+                 bg="#8BC49A", fg="black", bd=1, command=callgreedyv)
+button2.grid(row=4, column=0, columnspan=3, sticky=NSEW)
+
+button3 = Button(frame_left, text="Greedy Weight",
+                 bg="#8BC49A", fg="black", bd=1, command=callgreedyw)
+button3.grid(row=5, column=0, columnspan=3, sticky=NSEW)
+
+button4 = Button(frame_left, text="Greedy Ratio",
+                 bg="#8BC49A", fg="black", bd=1, command=callgreedyr)
+button4.grid(row=6, column=0, columnspan=3, sticky=NSEW)
+
+button5 = Button(frame_left, text="Dynamic", bg="#8BC49A",
+                 fg="black", bd=1, command=callDynamic)
+button5.grid(row=7, column=0, columnspan=3, sticky=NSEW)
+
+button6 = Button(frame_left, text="Branch and Bound",
+                 bg="#8BC49A", fg="black", bd=1, command=callBranchAndBound)
+button6.grid(row=8, column=0, columnspan=3, sticky=NSEW)
+
+button7 = Button(frame_left, text="Fully polynomial time",
+                 bg="#8BC49A", fg="black", bd=1)
+button7.grid(row=9, column=0, columnspan=3, sticky=NSEW)
+
+button8 = Button(frame_left, text="Randomized approach",
+                 bg="#8BC49A", fg="black", bd=1)
+button8.grid(row=10, column=0, columnspan=3, sticky=NSEW)
+
+button9 = Button(frame_left, text="Ant colony",
+                 bg="#8BC49A", fg="black", bd=1)
+button9.grid(row=11, column=0, columnspan=3, sticky=NSEW)
+
+button10 = Button(frame_left, text="Personnal algorithm",
+                  bg="#8BC49A", fg="black", bd=1)
+button10.grid(row=12, column=0, columnspan=3, sticky=NSEW)
+
+
+# determine a grid of 12*12 in the right frame
+for i in range(12):
+    frame_right.columnconfigure(i, weight=1)
+    frame_right.rowconfigure(i, weight=1)
+
+# creation of the title
+title1 = Label(frame_left, text="Maximum Value",
+               bg="#207436", fg="black", bd=1, relief=RIDGE, font=25)
+title1.grid(row=0, column=3, rowspan=2, columnspan=2, sticky=NSEW)
+
+title2 = Label(frame_left, text="Time of algorithme", bg="#207436",
+               fg="black", bd=1, relief=RIDGE, font=25)
+title2.grid(row=0, column=5, rowspan=2, columnspan=2, sticky=NSEW)
+
+
+# Create 11 label for the row 2 to 12 and that take all column
+for i in range (10):
+    Label(frame_left, text="", bg="#207436", fg="black", bd=1, relief=RIDGE).grid(
+        row=i+3, column=3, columnspan=2, sticky=NSEW)
+
+for i in range (10):
+    Label(frame_left, text="", bg="#207436", fg="black", bd=1, relief=RIDGE).grid(
+        row=i+3, column=5, columnspan=2, sticky=NSEW)
+# Print the window
+window.mainloop()
