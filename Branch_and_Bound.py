38e71978168182e606ee7f78cf1199c0ef8f7885
jeoffrey42 <jeoffrey.pereira>
2022-11-30 15:56:25 +0100

    commentaire et optimisation

--- Branch_and_Bound.py
+++ Branch_and_Bound.py
@@ -5,13 +5,12 @@
 
 # Node of the tree and then the subtree of items could be ignored with 2 conditions:
 # I] The left node include an item that overload the sack then the node his ignored
-# II] The max value of the right node subtree is under the best solution already found so all the subtree his ignored
+# II] The max value of the right node subtree is under the best solution already found so all the subtree are ignored
 
 #---------------------------------------------------START OF THE PROGRAM--------------------------------------------------------------#
 
 #---------------------------------------------------------IMPORT----------------------------------------------------------------------#
 
-from sys import argv
 from traceback import print_tb
 import ressources
 import time
@@ -19,16 +18,18 @@ import time
 #----------------------------------------------------USEFULL-FUNCTION-----------------------------------------------------------------#
 
 # This fonction take the first series of item in the items list to generate a fake best solution with a fake best value (maybe not fake if we are really lucky)
-# Then we can use this best combination list and best value to compare it in our main algorithm for the first loop
+# Then we can use this best combination list and best value to compare it in our main algorithm
 
 def create_Best_Combination(best_Combination,capacity,items):
     best_Weight = 0
     best_Value = 0
     i = 0
+    # We create a fake best combination with the first items of the list
     while (best_Weight + getattr(items[i], 'weight')) < capacity:
         best_Combination.append(items[i])
         best_Weight += getattr(items[i], 'weight')
         best_Value += getattr(items[i], 'value')
+        # cancel exeption when there is only one item in the list
         if i < len(items) - 1:
             i += 1
 
@@ -36,20 +37,25 @@ def create_Best_Combination(best_Combination,capacity,items):
 
 
 def knapsack(tree, capacity, best_Value, best_Weight, best_Combination, partial_Combination, partial_Weight, partial_Value):
-
+    # this is whene we find a better solution
+    # we update the best combination,the best value and the best weight
     if tree.CumValue > best_Value and tree.CumWeight <= capacity:
         best_Value = tree.CumValue
         best_Weight = tree.CumWeight
         best_Combination = partial_Combination.copy()
-    # Parcourir l'arbre
-    if tree.Left_children != None and tree.data != None:
+    # we check if the left node is not empty and if the left node is not overloading the sack
+    # then we try to add the item of the left node to the partial combination
+    if tree.Left_children != None and tree.data != None and tree.Left_children.CumWeight <= capacity:
             partial_Combination.append(tree.data)
             partial_Weight += tree.data.weight
             partial_Value += tree.data.value
+            #recursive call
             best_Combination, best_Value, best_Weight = knapsack(
                 tree.Left_children, capacity, best_Value, best_Weight, best_Combination, partial_Combination, partial_Weight, partial_Value)
             partial_Combination.pop()
+    # we check if the right node is not empty and if the right node is not under the best value already found
     if tree.Right_children != None and tree.Right_children.MaxValue > best_Value and tree.data != None:
+            #recursive call
             best_Combination, best_Value, best_Weight = knapsack(
                 tree.Right_children, capacity, best_Value, best_Weight, best_Combination, partial_Combination, partial_Weight, partial_Value)
 
@@ -67,24 +73,32 @@ def main(type, path):
     Tree = ressources.Node(None,None,None, None, None, None, None)
     # The Combination list with the best Value
     best_Combination = []
+    # The partial Combination list
     partial_Combination = []
+    # the partial weight and value
     partial_Weight = 0
     partial_Value = 0
 
 
     if type == "simple":
-        # Here we get the number of items the knapsack capacity and a list of items composed of positioning in the texte,value and weight.
+        # start the timer
         start = time.time()
+        # Here we get the number of items the knapsack capacity and a list of items composed of positioning in the texte,value and weight.
         nbrItems, capacity, items = ressources.readFileCreateList(path)
+        # We sort the list of items by value/weight ratio
         items.sort(key=lambda x: x.ratio, reverse=True)
-        # There we add each item of the list inside the tree and compute the value of all items.
+        # We stock the max value of all items
         for i in items:
             maxValue += i.value
+        # There we add each item of the list inside the tree and compute the value of all items.
         for data in items:
             ressources.Node.addNode(Tree, data, 0, maxValue, 0, None, None,nbrItems)
+        # We create a fake best combination with the first items of the list
         best_Combination, best_Value, best_Weight = create_Best_Combination(best_Combination,capacity,items)
+        # We start the recursive function to find the best combination
         best_Combination, best_Value, best_Weight = knapsack(
             Tree, capacity, best_Value, best_Weight, best_Combination, partial_Combination, partial_Weight, partial_Value)
+        # stop the timer
         start = time.time() - start
     if type == "multi":
         best_Value = 0
